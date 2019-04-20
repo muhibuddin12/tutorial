596d6acceb5bfdb9bced174b98bed0dc56e5e437
Muhibuddin <30959389+muhibuddin12@users.noreply.github.com>
2019-04-20 22:20:05 +0700

    ini test 2

--- index.php
+++ index.php
@@ -12,13 +12,6 @@
     <ul>
         <li><a href="http://"></a></li>
     </ul>
-    <h1>Nama Saya Muhibuddin</h1>
-    <h2>Ini tambahan lagi yaa</h2>
-    <h3>Ini Budi</h3>
-    <h1>Nama Saya Muhibuddin</h1>
-    <h2>Ini tambahan lagi yaa</h2>
-    <h3>Ini Budi</h3>
-
     <h3>Ini Budi</h3>
     <h2>Ini tambahan lagi yaa</h2>
     <h3>Ini Budi</h3>

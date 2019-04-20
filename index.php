21fddb8562085c8cf5bdc044df9f6a447c371eb0
Muhibuddin <30959389+muhibuddin12@users.noreply.github.com>
2019-04-20 15:19:15 +0700

    Belajar github

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<h1>Nama Saya Muhibuddin</h1>
-<h2>Ini tambahan lagi yaa</h2>
-<h3>Ini Budi</h3>
-
-<h1>Nama Saya Muhibuddin</h1>
-<h2>Ini tambahan lagi yaa</h2>
-<h3>Ini Budi</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Belajar html</title>
+</head>
+<body>
+    <ul>
+        <li><a href="http://"></a></li>
+        <li><a href="http://"></a></li>
+        <li><a href="http://"></a></li>
+        <li><a href="http://"></a></li>
+        <li><a href="http://"></a></li>
+    </ul>
+</body>
+</html>
